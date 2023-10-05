e2665ef59e481720ca6951c4c81c9d1a0012ff5f
forte <forte@DESKTOP-GENT8EK>
2023-10-05 20:07:05 +0200

    Add project files.

---
+++ ASP_MVC_App/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ASP_MVC_App.Models;
+
+namespace ASP_MVC_App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ASP_MVC_App.Models.Joke>? Joke { get; set; }
+    }
+}
